b7261f22ecda1eba6597ca540c1a0c7d7dd006a9
Jagadeesh Krishnanjanappa <jagadeesh@alifsemi.com>
2024-02-16 22:20:34 +0530

    LAPC-630 arch:arm: Add defconfig and DTS for Devkit E5
    Source: Alif Semiconductor
    JIRA ID: LAPC-630
    Type: Development
    Disposition: Local
    Description:
    v2 change:
    Update vendor-prefixes.yaml file.
    
    Reviewed-by: Hemanth Kumar <hemanth@alifsemi.com>
    Reviewed-by: Harith George <harith.g@alifsemi.com>
    Signed-off-by: Jagadeesh Krishnanjanappa <jagadeesh@alifsemi.com>
    Change-Id: I707c6219e801c8dcf07cb39e0740238b0d9a4e48

---
+++ arch/arm/boot/dts/devkit_e5_dct_defines.h
@@ -0,0 +1,129 @@
+/* SPDX-License-Identifier: GPL-2.0
+ *
+ * Copyright (C) 2024 Alif Semiconductor - All Rights Reserved.
+ * Use, distribution and modification of this code is permitted under the
+ * terms stated in the Alif Semiconductor Software License Agreement
+ *
+ * You should have received a copy of the Alif Semiconductor Software
+ * License Agreement with this file. If not, please write to:
+ * contact@alifsemi.com, or visit: https://alifsemi.com/license
+ *
+ * Author: Harith George <harith.g@alifsemi.com>
+ */
+
+/* HWSEM defines */
+#define HWSEM0_STATUS "disabled"
+#define HWSEM1_STATUS "disabled"
+#define HWSEM2_STATUS "disabled"
+#define HWSEM3_STATUS "disabled"
+#define HWSEM4_STATUS "disabled"
+#define HWSEM5_STATUS "disabled"
+#define HWSEM6_STATUS "disabled"
+#define HWSEM7_STATUS "disabled"
+#define HWSEM8_STATUS "disabled"
+#define HWSEM9_STATUS "disabled"
+#define HWSEM10_STATUS "disabled"
+#define HWSEM11_STATUS "disabled"
+#define HWSEM12_STATUS "disabled"
+#define HWSEM13_STATUS "disabled"
+#define HWSEM14_STATUS "disabled"
+#define HWSEM15_STATUS "disabled"
+
+/* MHU defines */
+#define RTSS_HP_MHU0_STATUS "okay"
+#define RTSS_HP_MHU1_STATUS "okay"
+#define RTSS_HE_MHU0_STATUS "okay"
+#define RTSS_HE_MHU1_STATUS "okay"
+#define SE_MHU0_STATUS "okay"
+#define SE_MHU1_STATUS "okay"
+
+/* Ethernet defines */
+#define ETH_STATUS "disabled"
+
+/* SDMMC defines */
+#define SDHCI_STATUS "disabled"
+
+/* SPI defines */
+#define SPI0_STATUS "disabled"
+#define SPI1_STATUS "disabled"
+#define SPI2_STATUS "disabled"
+#define SPI3_STATUS "okay"
+
+/* UART defines */
+#define UART0_STATUS "disabled"
+#define UART1_STATUS "disabled"
+#define UART2_STATUS "okay"
+#define UART3_STATUS "disabled"
+#define UART4_STATUS "disabled"
+#define UART5_STATUS "disabled"
+#define UART6_STATUS "disabled"
+#define UART7_STATUS "disabled"
+
+/* USB defines */
+#define HSUSB_STATUS "disabled"
+
+/* UTIMER defines */
+#define UTIMER0_STATUS "disabled"
+#define UTIMER1_STATUS "disabled"
+#define UTIMER2_STATUS "disabled"
+#define UTIMER3_STATUS "disabled"
+#define UTIMER4_STATUS "disabled"
+#define UTIMER5_STATUS "disabled"
+#define UTIMER6_STATUS "disabled"
+#define UTIMER7_STATUS "disabled"
+#define UTIMER8_STATUS "disabled"
+#define UTIMER9_STATUS "disabled"
+#define UTIMER10_STATUS "disabled"
+#define UTIMER11_STATUS "disabled"
+
+/* DSI defines */
+#define DSI_STATUS "disabled"
+
+/* DPI defines */
+#define DPI_STATUS "disabled"
+
+/* CSI defines */
+#define CSI_STATUS "disabled"
+
+/* CPI defines */
+#define CPI_STATUS "disabled"
+
+/* ADC defines */
+#define ADC120_STATUS "disabled"
+#define ADC121_STATUS "disabled"
+#define ADC122_STATUS "disabled"
+
+/* CMP defines */
+#define CMP0_STATUS "disabled"
+#define CMP1_STATUS "disabled"
+#define CMP2_STATUS "disabled"
+#define CMP3_STATUS "disabled"
+
+/* DAC defines */
+#define DAC120_STATUS "disabled"
+#define DAC121_STATUS "disabled"
+#define DAC122_STATUS "disabled"
+
+/* I2C Defines */
+#define I2C0_STATUS "disabled"
+#define I2C1_STATUS "disabled"
+#define I2C2_STATUS "disabled"
+#define I2C3_STATUS "disabled"
+
+/* I3C Defines */
+#define I3C0_STATUS "disabled"
+
+/* I2S Defines */
+#define I2S0_STATUS "disabled"
+#define I2S1_STATUS "disabled"
+#define I2S2_STATUS "disabled"
+#define I2S3_STATUS "disabled"
+
+/* CDC200 Defines */
+#define CDC200_STATUS "okay"
+
+/* D2D Defines */
+#define DAVE_2D_STATUS "okay"
+
+/* PCM Defines */
+#define PCM_STATUS "disabled"
